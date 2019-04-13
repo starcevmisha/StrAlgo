7a486c4445cc5d0b43a4a5d29e093d354579c758
starcev_misha <starcev_misha@mail.ru>
2019-04-13 21:58:48 +0500

    C;bvftn ghfdbkmyj

--- LZW/LZW.cs
+++ LZW/LZW.cs
@@ -5,52 +5,86 @@ using System.Text;
 
 namespace task_LZW
 {
-	class LZW
-	{
-		private static int[] DataToLZW(byte[] data)
-		{
-			throw new NotImplementedException();
-		}
-		
-		private static byte[] LZWtoData(int[] lzw)
-		{
-			throw new NotImplementedException();
-		}
-		
-		public static byte[] Compress(byte[] data)
-		{
-			var lzw = DataToLZW(data);
-			var output = new List<byte>();
-			int id_bits = 8, curr_bit = 0;
-			for (int id = 0; id < lzw.Length; id++)
-			{
-				int new_size = (curr_bit + id_bits + 7) / 8 + 8;
-				output.AddRange(new byte[new_size - output.Count]);
-				var bytes = BitConverter.GetBytes((ulong)lzw[id] << curr_bit % 8);
-				for (int k = 0; k < 2 + id_bits/8; k++)
-					output[curr_bit/8 + k] |= bytes[k];
-				curr_bit += id_bits;
-				if (256 + id >= (1 << id_bits))
-					id_bits++;
-			}
-			output.RemoveRange((curr_bit + 7) / 8, 8);
-			return output.ToArray();
-		}
-		
-		public static byte[] Decompress(byte[] data)
-		{
-			var lzw = new List<int>();
-			int id_bits = 8, curr_bit = 0, last_bit = data.Length * 8;
-			data = data.Concat(new byte[8]).ToArray();
-			for (int id = 0; curr_bit + id_bits <= last_bit; id++)
-			{
-				ulong x = BitConverter.ToUInt64(data, curr_bit / 8);
-				lzw.Add((int)(x >> curr_bit % 8) & (1 << id_bits) - 1);
-				curr_bit += id_bits;
-				if (256 + id >= (1 << id_bits))
-					id_bits++;
-			}
-			return LZWtoData(lzw.ToArray());
-		}
-	}
+    class LZW
+    {
+        private static int[] DataToLZW(byte[] data)
+        {
+            var dict = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
+            {
+                    dict.Add(new string((char)i, 1), i);
+            }
+                       
+            var outArray = new List<int>();
+
+            var w = "";
+
+            foreach (var c in data)
+            {
+                var wc = w + new string((char)c, 1);
+                if (dict.ContainsKey(wc.ToString()))
+                {
+                    w = wc;
+                }
+                else
+                {
+                    outArray.Add(dict[w]);
+                    dict.Add(wc, dict.Count);
+
+                    w = new string((char)c, 1);
+                }
+            }
+
+            if (w.Length != 0)
+            {
+                outArray.Add(dict[w.ToString()]);
+            }
+
+
+            return outArray.ToArray();
+        }
+
+        private static byte[] LZWtoData(int[] lzw)
+        {
+
+        }
+
+        public static byte[] Compress(byte[] data)
+        {
+            var lzw = DataToLZW(data);
+            var output = new List<byte>();
+            int id_bits = 8, curr_bit = 0;
+            for (int id = 0; id < lzw.Length; id++)
+            {
+                int new_size = (curr_bit + id_bits + 7) / 8 + 8;
+                output.AddRange(new byte[new_size - output.Count]);
+                var bytes = BitConverter.GetBytes((ulong) lzw[id] << curr_bit % 8);
+                for (int k = 0; k < 2 + id_bits / 8; k++)
+                    output[curr_bit / 8 + k] |= bytes[k];
+                curr_bit += id_bits;
+                if (256 + id >= (1 << id_bits))
+                    id_bits++;
+            }
+
+            output.RemoveRange((curr_bit + 7) / 8, 8);
+            return output.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            var lzw = new List<int>();
+            int id_bits = 8, curr_bit = 0, last_bit = data.Length * 8;
+            data = data.Concat(new byte[8]).ToArray();
+            for (int id = 0; curr_bit + id_bits <= last_bit; id++)
+            {
+                ulong x = BitConverter.ToUInt64(data, curr_bit / 8);
+                lzw.Add((int) (x >> curr_bit % 8) & (1 << id_bits) - 1);
+                curr_bit += id_bits;
+                if (256 + id >= (1 << id_bits))
+                    id_bits++;
+            }
+
+            return LZWtoData(lzw.ToArray());
+        }
+    }
 }
